a8dabe57133fd957ccff6e65e29909f9e83bf652
binoy638 <binoy638@gmail.com>
2021-04-17 21:27:55 +0530

    added redis and file upload

--- colorExt.py
+++ colorExt.py
@@ -1,22 +1,25 @@
 import requests
 import extcolors
 import os
+import uuid
 
 temp_dir = "temp"
 
 if not os.path.exists(temp_dir):
     os.mkdir(temp_dir)
 
-def getRBGarray(tupleArray):
+def getRBGarray(tupleArray,pixel_count):
     colors = []
     for color in tupleArray:
-        colors.append(str(color[0]))
+        percent = color[1]/pixel_count * 100
+        colors.append([str(color[0]),round(percent,2)])
     return colors
 
-def downloadImage(URL,name):
+def downloadImage(URL):
     try:
         r = requests.get(URL)
         if r.status_code == 200:
+            name = uuid.uuid4()
             path = f"{temp_dir}/{name}.jpg"
             with open(path, "wb") as f:
                 f.write(r.content)
@@ -30,7 +33,7 @@ def downloadImage(URL,name):
 
 def extractColors(path):
     colors, pixel_count = extcolors.extract_from_path(path)
-    return getRBGarray(colors) 
+    return getRBGarray(colors,pixel_count) 
 
 def cleanUp(path):
     if os.path.exists(path):
@@ -38,8 +41,9 @@ def cleanUp(path):
     else:
         print(f"{path} not found")
 
-def getColorFromImage(url,name):
-    path = downloadImage(url,name)
+
+def getColorFromImage(url):
+    path = downloadImage(url)
     #download and save the image
     if not path:
         return None
@@ -49,3 +53,11 @@ def getColorFromImage(url,name):
     cleanUp(path)                    
     return colors                     
 
+def getColorFromImage_(path):
+    if not path:
+        return None
+    #extract colors from the image
+    colors = extractColors(path)  
+    #delete the image        
+    cleanUp(path)                    
+    return colors                     
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from fastapi import Body,FastAPI
-from colorExt import getColorFromImage
+from fastapi import Body,FastAPI, File, UploadFile, HTTPException
+from colorExt import getColorFromImage,getColorFromImage_
+from dotenv import load_dotenv
+import shutil
+import os
+import json
+import redis
 
+load_dotenv()
 app = FastAPI()
+cache = redis.from_url(url=os.environ.get("URL"),decode_responses=True)
+
 
 @app.get("/")
 def read_root():
     return {"Status": "Server is ready"}
 #https://i.scdn.co/image/ab67616d00001e02ed3faf75cfde73ebb4734162
-@app.post("/color")
+@app.post("/color",status_code=200)
 def getColor(payload: dict = Body(...)):
-    colors = getColorFromImage(payload["url"],payload["name"])
+    url = payload["url"]
+    isCached = cache.get(url)
+    if isCached:
+        colors = json.loads(isCached)
+    else:
+        colors = getColorFromImage(url)
+        if colors:
+            cache.set(url,json.dumps(colors))
+        else:
+            raise HTTPException(status_code=404, detail="Could not download image")
+    return {"colors":colors}
+
+@app.post("/image",status_code=200)
+async def image(image: UploadFile = File(...)):
+    path = f"temp/{image.filename}"
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    colors = getColorFromImage_(path)
     if not colors:
-        return {"StatusCode":404}
-    return {"StatusCode":200,"data":colors}
+        raise HTTPException(status_code=500, detail="Could not process image")
+    return {"colors":colors}
